1a652891c0b6cd41ff9843b408752c173c89dc0b
David Hülsberg <46696514+david-h-dev@users.noreply.github.com>
2020-08-17 17:49:08 +0200

    Initial commit

---
+++ ConsoleAppSchleifen/ConsoleAppSchleifen/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSchleifen
+{
+    class Program
+    {
+        private static int zähler = 50;
+        private static int i;
+
+        static void Main(string[] args)
+        {
+            while (zähler >= 40)
+            {
+                Console.WriteLine("Die Glühbirne hat " + zähler + " Watt.");
+                zähler--;
+            }
+            Console.WriteLine("__________________________________");
+            Console.WriteLine();
+            do
+            {
+                Console.WriteLine("Die Glühbirne hat " + zähler + " Watt.");
+                zähler++;
+            }
+            while (zähler <= 60);
+
+            Console.WriteLine("__________________________________");
+            Console.WriteLine();
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Die Glühbirne hat " + i + " Watt.");
+            }
+            Console.WriteLine("__________________________________");
+            Console.ReadKey();
+        }
+    }
+}
